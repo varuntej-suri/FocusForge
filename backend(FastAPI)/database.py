4c917cfe54e5c4318de26875a4cfc6b87f90ce4e
varun <varunsuri50013@gmail.com>
2026-08-06 23:57:15 +0530

    feat: migrate backend from SQLite to PostgreSQL

--- backend(FastAPI)/database.py
+++ backend(FastAPI)/database.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = "postgresql+psycopg2://postgres:varun3@localhost:5432/focusforge"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
